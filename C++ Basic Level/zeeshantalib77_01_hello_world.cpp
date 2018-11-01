13aeaca5f2fc0260f8ff4c6e6d44c8956868b1f8
Zeeshan Talib <zeeshantalib77@yahoo.com>
2018-11-01 14:21:09 +0500

    Update zeeshantalib77_01_hello_world.cpp

--- C++ Basic Level/zeeshantalib77_01_hello_world.cpp
+++ C++ Basic Level/zeeshantalib77_01_hello_world.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    cout<<"hello world \n";                // this text will be show on screen
+    cout<<"hello world \n";                // this text will be shown on screen
     cout<<"My name is Zeeshan \n";         //  newline - \n
     cout<<"My name is"<<" "<<"Zeeshan";
     return 0;
